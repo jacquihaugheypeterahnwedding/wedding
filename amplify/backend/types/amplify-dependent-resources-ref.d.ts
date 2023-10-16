7999670ee36b8d0496238179d4d1d96f4e5913d7
jacquihaugheypeterahnwedding <jacquihaugheypeterahnwedding@gmail.com>
2023-10-16 11:00:51 -0400

    add data model

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "wedding": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "wedding": {
       "AppClientID": "string",
